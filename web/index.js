fea5649efb630ae33e054448345afd131b0f4f2c
vicanso <vicansocanbico@gmail.com>
2021-12-26 09:02:24 +0800

    refactor: adjust charts preview

--- web/index.js
+++ web/index.js
@@ -8,7 +8,22 @@ editor.setSize("100%", height);
 
 function run() {
     var option = editor.getValue();
-    axios.post("/", JSON.parse(option)).then(function(resp) {
+    var data = null;
+    try {
+        if (option.indexOf("option = ") !== -1) {
+            var fn = new Function("var " + option + ";return option;");
+            data = fn();
+        } else {
+            data = JSON.parse(option);
+        }
+    } catch (err) {
+        alert(err.message);
+        return;
+    }
+
+    axios.post("/", data).then(function(resp) {
         document.getElementById("svg").innerHTML = resp;
+    }).catch(function(err) {
+        alert(err.message);
     });
 }
